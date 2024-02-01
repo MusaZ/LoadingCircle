5a56253f79e6571748400d8e508e53f9fbe31959
Musa.Z <alayci54@hotmail.com>
2024-02-01 19:49:39 +0300

    Change Method

--- General.Blazor.App/wwwroot/js/devxpopup.js
+++ General.Blazor.App/wwwroot/js/devxpopup.js
@@ -1,10 +1,22 @@
-﻿function getSizeDevXPopup()
-{
-    let dxmodal = document.getElementsByClassName('dxbl-popup dxbl-popup-adaptivity')[0];
-    let height = dxmodal.clientHeight;
-    let width = dxmodal.clientWidth;
-    console.log(height);
-    console.log(width);
-    return [width.toString(), height.toString()];
-}
+﻿document.addEventListener('DOMContentLoaded',()=>{
+   var timer;
+   function startTimer()
+   {
+       if(timer === undefined || timer === null)
+        timer = setInterval(getSizeDevXPopup, 1000);
+   }
 
+    function getSizeDevXPopup()
+    {
+        let dxmodal = document.getElementsByClassName('dxbl-popup dxbl-popup-adaptivity')[0];
+        if(dxmodal !== undefined)
+        {
+            let height = dxmodal.clientHeight;
+            let width = dxmodal.clientWidth;
+            console.log(height);
+            console.log(width);
+            clearInterval(timer);
+            return [width.toString(), height.toString()];
+        }
+    }
+});
