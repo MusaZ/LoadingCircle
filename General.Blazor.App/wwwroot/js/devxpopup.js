5fd63f3c87aec6d87944c32f74b575ee1c95acd4
Musa.Z <alayci54@hotmail.com>
2024-02-01 20:20:37 +0300

    Completed Loading Circle For DevXPopup Component

--- General.Blazor.App/wwwroot/js/devxpopup.js
+++ General.Blazor.App/wwwroot/js/devxpopup.js
@@ -1,22 +1,30 @@
-﻿document.addEventListener('DOMContentLoaded',()=>{
-   var timer;
-   function startTimer()
-   {
-       if(timer === undefined || timer === null)
-        timer = setInterval(getSizeDevXPopup, 1000);
-   }
+﻿var timer;
+function startTimer()
+{
+  // if(timer === undefined || timer === null)
+  //   timer = setInterval(getSizeDevXPopup, 1000);
+}
 
-    function getSizeDevXPopup()
+function getSizeDevXPopup()
+{
+  let dxmodal = document.getElementsByClassName('dxbl-popup dxbl-popup-adaptivity')[0];
+  if(dxmodal !== undefined)
+  {
+    let height = dxmodal.clientHeight;
+    let width = dxmodal.clientWidth;
+    console.log(height);
+    console.log(width);
+    clearInterval(timer);
+    return [width.toString(), height.toString()];
+    
+    let loading = document.getElementsByClassName('loading-div')[0];
+    if(loading !== undefined || loading !== null)
     {
-        let dxmodal = document.getElementsByClassName('dxbl-popup dxbl-popup-adaptivity')[0];
-        if(dxmodal !== undefined)
-        {
-            let height = dxmodal.clientHeight;
-            let width = dxmodal.clientWidth;
-            console.log(height);
-            console.log(width);
-            clearInterval(timer);
-            return [width.toString(), height.toString()];
-        }
+      loading.style.width = width.toString() + "px";
+      loading.style.height = height.toString() + "px";
+      loading.style.display="grid";
+      clearInterval(timer);
+      return [width.toString(), height.toString()];
     }
-});
+  }
+}
