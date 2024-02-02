68bde7516454a0989556007ff6838918af749a76
Musa.Z <alayci54@hotmail.com>
2024-02-02 22:44:13 +0300

    Added Loading Panel into DevXGrid

--- General.Blazor.App/wwwroot/js/devxpopup.js
+++ General.Blazor.App/wwwroot/js/devxpopup.js
@@ -1,12 +1,15 @@
-﻿function getSizeDevXPopup()
+﻿function getSizeDevXPopup(className)
 {
-  let dxmodal = document.getElementsByClassName('dxbl-popup dxbl-popup-adaptivity')[0];
+  //let dxmodal = document.getElementsByClassName('dxbl-popup dxbl-popup-adaptivity')[0];
+  let dxmodal = document.getElementsByClassName(className)[0];
   if(dxmodal !== undefined)
   {
     let height = dxmodal.clientHeight;
     let width = dxmodal.clientWidth;
+    let left = dxmodal.offsetLeft;
+    let top=dxmodal.offsetTop;
     console.log(height);
     console.log(width);
-    return [width.toString(), height.toString()];
+    return [width.toString(), height.toString(), left.toString(), top.toString()];
   }
 }
