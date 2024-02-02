6781f29dd858c9847ce7ddbe446cb9c1d1774135
MusaZ <alayci54@hotmail.com>
2024-02-02 18:00:34 +0300

    Added For DevXGrid

--- General.Blazor.App/wwwroot/js/devxpopup.js
+++ General.Blazor.App/wwwroot/js/devxpopup.js
@@ -1,11 +1,4 @@
-﻿var timer;
-function startTimer()
-{
-  // if(timer === undefined || timer === null)
-  //   timer = setInterval(getSizeDevXPopup, 1000);
-}
-
-function getSizeDevXPopup()
+﻿function getSizeDevXPopup()
 {
   let dxmodal = document.getElementsByClassName('dxbl-popup dxbl-popup-adaptivity')[0];
   if(dxmodal !== undefined)
@@ -14,17 +7,6 @@ function getSizeDevXPopup()
     let width = dxmodal.clientWidth;
     console.log(height);
     console.log(width);
-    clearInterval(timer);
     return [width.toString(), height.toString()];
-    
-    let loading = document.getElementsByClassName('loading-div')[0];
-    if(loading !== undefined || loading !== null)
-    {
-      loading.style.width = width.toString() + "px";
-      loading.style.height = height.toString() + "px";
-      loading.style.display="grid";
-      clearInterval(timer);
-      return [width.toString(), height.toString()];
-    }
   }
 }
